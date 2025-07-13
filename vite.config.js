b428ab95f8cee340b198a7832259651198de0032
Ishaan_Sawant <imsawant05@gmail.com>
2025-07-14 00:51:29 +0530

    feat: site tite, favicon, network hosting

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // OR '0.0.0.0'
+    port: 5173,
+    allowedHosts: ['.localhost', '.ark.ive', '.zenmaster']
+  }
 })
